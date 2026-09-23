418d915d14f16cf610d1cbbdd54f25c9e8083b1a
LAW XING HONG <lawxinghong@gmail.com>
2026-09-24 01:27:16 +0800

    T-tocontrol
    justkey

--- Assets/Script/HONGScript/Scanning/GridSystem.cs
+++ Assets/Script/HONGScript/Scanning/GridSystem.cs
@@ -23,7 +23,7 @@ public class GridSystem : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.T))
+        if (Input.GetKeyDown(KeyCode.LeftControl))
         {
             if (!GridOpen)
             {
